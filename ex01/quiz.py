f1985c43a2a7e0ddfbabb333df6bbe937a7a8fb4
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-11-22 13:58:36 +0900

    問題データを入力しました

---
+++ ex01/quiz.py
@@ -0,0 +1,14 @@
+#ランダムに問題を出題する関数
+def shutudai():
+    quiz_data = {
+        "quiz1":{"question": "サザエさんの旦那の名前は?", "answer":["マスオ", "ますお"]},
+        "quiz2":{"question": "カツオの妹の名前は?", "answer":["ワカメ", "わかめ"]},
+        "quiz3":{"question": "タラオはカツオから見てどんな関係?", "answer":["甥", "おい", "甥っ子", "おいっこ"]}
+                }
+
+#解答を受け付け評価する関数
+def kaitou():
+    pass
+
+if __name__ == "__main__":
+    pass
