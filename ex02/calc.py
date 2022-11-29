3b195b6d60636235b04b109a04269abe6de6af15
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-11-29 14:01:50 +0900

    add +button and =button

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,16 +10,26 @@ root.geometry("300x500")
 def button_click(e):
     btn = e.widget
     txt = btn["text"]
-    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+    #entry.insert(tk.END, txt)
 
-
+# テキスト入力欄追加
 entry = tk.Entry(justify="right", width=10, font=40)
 entry.grid(row=0, column=0, columnspan=3)
 
-# ボタン配置
+# 数字ボタン配置
 for i in range(10):
     button = tk.Button(root, text=i, width=4, height=2, font=30)
     button.bind("<1>", button_click)
     button.grid(row=(12-i) // 3, column=(12-i) % 3)
+
+
+# +ボタン追加
+button = tk.Button(root, text="+", width=4, height=2, font=30)
+button.bind("<1>", button_click)
+button.grid(row=4, column=1)
+
+# =ボタン追加
+button = tk.Button(root, text="=", width=4, height=2, font=30)
+button.grid(row=4, column=2)
 
 root.mainloop()
