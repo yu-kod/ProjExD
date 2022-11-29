93d910d678e415626bd4c0c94c56782121fe0554
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-11-29 16:00:27 +0900

    記号を変更できるようになりました

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,7 +9,7 @@ class App:
         root.geometry("300x500")
 
         #計算式を表示するlabel
-        self.label = tk.Label(root, text="")
+        self.label = tk.Label(root, anchor=tk.NE, text="", font=("", 10))
         self.label.grid(row=0, column=0, columnspan=4)
 
         # テキスト入力欄追加
@@ -18,7 +18,7 @@ class App:
 
         # 数字ボタン配置
         for i in range(10):
-            button = tk.Button(root, text=i, width=4, height=2, font=("", 15))
+            button = tk.Button(root, text=i, width=5, height=2, font=("", 18))
             button.bind("<1>", self.button_click)
             button.grid(row=(3*7-i) // 3, column=(i-1) % 3)
 
@@ -26,13 +26,11 @@ class App:
         # 数字以外のボタン追加
         other_button = ["<-", "/", "*", "-", "+", "="]
         for i in range(len(other_button)):
-            button = tk.Button(root, text=other_button[i], width=4, height=2, font=("", 15))
+            button = tk.Button(root, text=other_button[i], width=5, height=2, font=("", 18))
             button.bind("<1>", self.other_click)
             button.grid(row=2+i, column=3)
-
-        root.mainloop()
     
-    # ボタンがクリックされたときの動作(メッセージボックス表示)
+    # 数字ボタンがクリックされたときの動作(メッセージボックス表示)
     def button_click(self, e):
         btn = e.widget
         txt = btn["text"]
@@ -41,13 +39,14 @@ class App:
         self.entry.insert(tk.END, txt)
         App.num_flag = True
 
+    # 数字以外のボタンを押したときの動作
     def other_click(self, e):
         btn = e.widget
         txt = btn["text"]
         if txt == "=":
             self.equal_click(e)
         elif txt == "<-":
-            pass
+            self.delete_one()
         else:
             if App.num_flag == True:
                 if self.label["text"] == "":
@@ -57,8 +56,11 @@ class App:
                         self.label["text"] = str(eval(self.label["text"] + self.entry.get())) + txt
                 App.num_flag = False
             else:
-                pass
-        
+                if txt in ["+", "-", "*", "/"]:
+                    self.label["text"] = self.label["text"][:-1] + txt
+    
+    def delete_one(self):
+        self.entry.delete(len(self.entry.get())-1, tk.END)
 
     # =ボタンがクリックされたときの動作
     def equal_click(self, e):
@@ -67,8 +69,6 @@ class App:
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, ans)
         self.label["text"] = ""
-
-
 
 
 def main():
