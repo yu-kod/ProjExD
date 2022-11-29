04ac9a1336f49bfee82dd244eebd1c32a4c26348
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-11-29 13:36:20 +0900

    add button event

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,9 +6,15 @@ root.title("calc")
 root.geometry("300x500")
 
 
+def button_click(e):
+    btn = e.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+
+
 for i in range(9, -1, -1):
     button = tk.Button(root, text=i, width=4, height=2, font=30)
-    #button.bind("<1>", button_click)
+    button.bind("<1>", button_click)
     button.grid(row=i // 3, column=i % 3)
 
 root.mainloop()
