cab057b4c6a6ba64bde9d88736e37aa9cc495561
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-11-29 13:35:25 +0900

    add button

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,14 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("calc")
 root.geometry("300x500")
+
+
+for i in range(9, -1, -1):
+    button = tk.Button(root, text=i, width=4, height=2, font=30)
+    #button.bind("<1>", button_click)
+    button.grid(row=i // 3, column=i % 3)
+
 root.mainloop()
