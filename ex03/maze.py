6efdc9afccfad2d87ac5bfb779b52487b46de601
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-12-06 13:18:26 +0900

    add ex03 and add maze.py

---
+++ ex03/maze.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+class App:
+    def __init__(self):
+        pass
+
+
+def main():
+    root = tk.Tk()
+    app = App(root)
+    app.root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
