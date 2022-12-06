b7bf4de0ecef0536ceedbae91a6b34af10f05252
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-12-06 16:45:17 +0900

    system completed (too lag)

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import maze_maker as mm
-import copy
+import pickle as cPickle
 
 
 class Application(tk.Frame):
@@ -18,25 +18,44 @@ class Application(tk.Frame):
         self.mx,self.my = 1, 1
         self.bird = self.canvas.create_image(150,150,image=self.image)              # 画像配置
 
+        self.HP_bar = self.canvas.create_rectangle(
+            0, 5, 1500, 20, fill="#00ff00")
+
         self.key = ""
         master.bind("<KeyPress>", self.key_down)                                    # key反応配置
         master.bind("<KeyRelease>", self.key_up)
 
-        self.timer = 0
-        self.main_proc()
-
         self.maze = mm.make_maze(15, 9)
+        self.maze_after1 = cPickle.loads(cPickle.dumps(self.maze, -1))
+        for i in range(1, 14):
+            for j in range(1, 8):
+                if (i + j) % 2 == 0:
+                    self.maze_after1[i][j] = 4
+        self.maze_after1[1][1] = 2
+        self.maze_after1[13][7] = 3
 
+        self.maze_after2 = cPickle.loads(cPickle.dumps(self.maze, -1))
+        for i in range(1, 14):
+            for j in range(1, 8):
+                if (i + j) % 2 == 1:
+                    self.maze_after2[i][j] = 4
+        self.maze_after2[1][1] = 2
+        self.maze_after2[13][7] = 3
+
+
+        self.timer = 0
+        self.HP = 300
         self.tick = 0
+        self.main_proc()
         self.rythm()
     
+
     def key_down(self,e):               #ボタンを押したとき、keyをself.keyに保存
         self.key = e.keysym
 
 
     def key_up(self,e):
         self.key = ""
-
 
     def main_proc(self):
         if self.timer == 0:
@@ -48,14 +67,22 @@ class Application(tk.Frame):
             self.timer = 0
         else:
             self.timer += 1
-            if self.timer >= 500:
+            if self.timer >= 25:
                 self.timer = 0
+        
+        if (self.tick == 1 and self.maze_after1[self.mx][self.my] == 4) or (self.tick == 0 and self.maze_after2[self.mx][self.my] == 4):
+            self.HP -= 1
+            if self.HP <= 0:
+                self.mx, self.my = 1, 1
+                self.HP = 300
 
         self.cx = self.mx*100 + 50
         self.cy = self.my*100 + 50
         self.canvas.coords(self.bird, self.cx, self.cy)             #計算後反映
+        self.canvas.coords(self.HP_bar, 0,5,1500*self.HP/300,20)
         self.canvas.tag_raise(self.bird)
-        self.master.after(1, self.main_proc)
+        self.canvas.tag_raise(self.HP_bar)
+        self.master.after(4, self.main_proc)
 
 
     def move_detect_wall(self, direction, num):                     #壁を判定しつつ移動処理を行う。
@@ -69,15 +96,12 @@ class Application(tk.Frame):
 
 
     def rythm(self):
-        maze_after = copy.deepcopy(self.maze)
-        for i in range(1,14):
-            for j in range(1,8):
-                if (i + j) % 2 == self.tick % 2:
-                    maze_after[i][j] = 4
-        maze_after[1][1] = 2
-        maze_after[13][7] = 3
-        mm.show_maze(self.canvas, maze_after)
-        self.tick += 1
+        if self.tick == 0:
+            mm.show_maze(self.canvas, self.maze_after1)
+            self.tick = 1
+        else:
+            mm.show_maze(self.canvas, self.maze_after2)
+            self.tick = 0
         self.master.after(500, self.rythm)
 
 
