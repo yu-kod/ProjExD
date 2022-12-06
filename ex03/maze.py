79e3c11c382ce12e0be2fb20f6d4444e7f188fb3
yu-kod <48035533+tasyumina-hito@users.noreply.github.com>
2022-12-06 13:47:09 +0900

    add key up down def

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,15 +1,38 @@
 import tkinter as tk
 
 
-class App:
-    def __init__(self):
+class Application(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.pack()
+
+        master.title("迷えるこうかとん")
+        master.resizable(width=False, height=False)
+
+        self.image = tk.PhotoImage(file="fig/2.png")
+        self.canvas = tk.Canvas(master, width=1500, height=900, bg="black")
+        self.canvas.pack()
+
+        self.cx = 300
+        self.cy = 400
+        self.canvas.create_image(self.cx,self.cy,image=self.image)
+
+        self.key = ""
+    
+    
+    def key_down(self,e):
+        pass
+
+    def key_up(self,e):
         pass
 
 
+
+
 def main():
-    root = tk.Tk()
-    app = App(root)
-    app.root.mainloop()
+    win = tk.Tk()
+    app = Application(master = win)
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
